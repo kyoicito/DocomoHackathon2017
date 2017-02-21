0fd9da4671619fd021baa7f87e2c5c96b4017fc5
mokemoketa <mokemoketa@gmail.com>
2017-02-21 15:59:18 +0900

    Camera Example

---
+++ app/src/main/java/docomofirstsky/thingpedia/MainActivity.java
@@ -0,0 +1,144 @@
+package docomofirstsky.thingpedia;
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.FileProvider;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity {
+
+    private final static int RESULT_CAMERA = 1001;
+    private final static int REQUEST_PERMISSION = 1002;
+
+    private ImageView imageView;
+    private Uri cameraUri;
+    private File  cameraFile;
+    private String filePath;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (savedInstanceState != null){
+            cameraUri = savedInstanceState.getParcelable("CaptureUri");
+        }
+
+        imageView = (ImageView)findViewById(R.id.image_view);
+
+        Button cameraButton = (Button)findViewById(R.id.camera_button);
+        cameraButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Android 6, API 23以上でパーミッシンの確認
+                if (Build.VERSION.SDK_INT >= 23) {
+                    checkPermission();
+                }
+                else {
+                    cameraIntent();
+                    //画像を送信
+                }
+            }
+        });
+    }
+
+    protected void onSaveInstanceState(Bundle outState){
+        outState.putParcelable("CaptureUri", cameraUri);
+    }
+
+    private void cameraIntent(){
+        // 保存先のフォルダーを作成
+        File cameraFolder = new File(
+                Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), "IMG"
+        );
+        cameraFolder.mkdirs();
+
+        // 保存ファイル名
+        String fileName = new SimpleDateFormat("ddHHmmss").format(new Date());
+        filePath = cameraFolder.getPath() +"/" + fileName + ".jpg";
+        Log.d("debug","filePath:"+filePath);
+
+        // capture画像のファイルパス
+        cameraFile = new File(filePath);
+//        cameraUri = Uri.fromFile(cameraFile);
+        cameraUri = FileProvider.getUriForFile(MainActivity.this, getApplicationContext().getPackageName() + ".provider", cameraFile);
+
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, cameraUri);
+        startActivityForResult(intent, RESULT_CAMERA);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        if (requestCode == RESULT_CAMERA) {
+
+            if(cameraUri != null){
+                imageView.setImageURI(cameraUri);
+            }
+            else{
+                Log.d("debug","cameraUri == null");
+            }
+        }
+
+    }
+
+    // Runtime Permission check
+    private void checkPermission(){
+        // 既に許可している
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)== PackageManager.PERMISSION_GRANTED){
+            cameraIntent();
+        }
+        // 拒否していた場合
+        else{
+            requestLocationPermission();
+        }
+    }
+
+    // 許可を求める
+    private void requestLocationPermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+            ActivityCompat.requestPermissions(MainActivity.this,
+                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_PERMISSION);
+
+        } else {
+            Toast toast = Toast.makeText(this, "許可されないとアプリが実行できません", Toast.LENGTH_SHORT);
+            toast.show();
+
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE,}, REQUEST_PERMISSION);
+
+        }
+    }
+
+    // 結果の受け取り
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode == REQUEST_PERMISSION) {
+            // 使用が許可された
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                cameraIntent();
+                return;
+
+            } else {
+                // それでも拒否された時の対応
+                Toast toast = Toast.makeText(this, "これ以上なにもできません", Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        }
+    }
+}
--- app/src/main/java/docomofirstsky/thingpedia/httpclient.java
+++
@@ -1,8 +0,0 @@
-package docomofirstsky.thingpedia;
-
-/**
- * Created by Hello on 2017/02/21.
- */
-
-public class httpclient {
-}
